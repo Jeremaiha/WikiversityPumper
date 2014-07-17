9236deea4cdf1b2342b73ce7049e945462d7e7bd
Jeremy Shiklov <myipodspain@gmail.com>
2014-07-17 03:31:25 +0300

    Update sql_writer.py

--- sql_writer.py
+++ sql_writer.py
@@ -1,9 +1,20 @@
 #functions which will write the queries
 
+def text_writer(file_name ,txt):
+	file_obj = open(file_name, 'w')
+	file_obj.write(txt)
+	file_obj.close()
+# End of text_writer
+
+def inject_c_challenges():
+  print "Starting c challenge query"
+  
+#end of inject c challenges
+
+def inject_cpp_challenges():
+  print "Starting c++ challenge query"  
+  
+#end of inject c++ challenges
 
 
-#main
-
-print "Jeremy was here!"
-
-print "I can upload data only from the webserver"
+#main 
