1e28ff9e30fe44ecb58d5e01182eeffa5080ee58
Jeremy Shiklov <myipodspain@gmail.com>
2014-07-17 04:09:26 +0300

    Update sql_writer.py

--- sql_writer.py
+++ sql_writer.py
@@ -6,20 +6,20 @@ def text_writer(file_name ,txt):
 	file_obj.close()
 # End of text_writer
 
-def inject_c_challenges():
-  print "Starting c challenge query"
+def inject_challenges(table_name):
+  print "Starting " + str(table_name) + " challenge query"
   
   insert_query = ""
   
   
   
-#end of inject c challenges
-
-def inject_cpp_challenges():
-  print "Starting c++ challenge query"  
-  
-#end of inject c++ challenges
+#end of inject challenges
 
 
 #main 
-list_tables = ['Android_Challenges.table','Assembly_Challenges.table','Blender_3D_Challenges.table','C_Challenges.table','CPP_Challenges.table','Encryption_Challenges.table','HTML_Challenges.table','Linux_Challenges.table','Math_Challenges.table','PHP_Challenges.table','Python_Challenges.table','Resources_Challenges.table','Reversing_Challenges.table','Security_Challenges.table','Thinking_Challenges.table'] #add here all file names with strings...
+list_tables = ['Android_Challenges.table','Assembly_Challenges.table','Blender_3D_Challenges.table','C_Challenges.table',
+		'CPP_Challenges.table','Encryption_Challenges.table','HTML_Challenges.table','Linux_Challenges.table',
+		'Math_Challenges.table','PHP_Challenges.table','Python_Challenges.table','Resources_Challenges.table',
+		'Reversing_Challenges.table','Security_Challenges.table','Thinking_Challenges.table']
+		
+		
